b5cab4ecdd2844d832d242e028ed07de979d9385
Johan Dahlin <jdahlin@gmail.com>
2026-01-04 20:51:01 +0100

    chore: add react plugin and aliases to vitest for better parsing

--- apps/desktop/vitest.config.ts
+++ apps/desktop/vitest.config.ts
@@ -1,4 +1,18 @@
+import { resolve } from 'node:path'
 import { config } from '@repo/vitest-config'
+import react from '@vitejs/plugin-react'
 import { defineConfig, mergeConfig } from 'vitest/config'
 
-export default defineConfig(mergeConfig(config, {}))
+export default defineConfig(
+  mergeConfig(config, {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        { find: '@renderer', replacement: resolve(__dirname, 'src/renderer') },
+        { find: '@repo/core', replacement: resolve(__dirname, '../../packages/core/src') },
+        { find: '@repo/shared', replacement: resolve(__dirname, '../../packages/shared/src') },
+        { find: '@repo/ux', replacement: resolve(__dirname, '../../packages/ux/src') },
+      ],
+    },
+  }),
+)
